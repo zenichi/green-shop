cf35d436a0d672caf9675d9a81e8328b682edf34
zenichi <krzysztof.kaczmarczyk@protonmail.com>
2022-07-16 14:01:26 +0200

    :factory: refactoring to make a test easier to read

--- green-api/internal/handlers/products_test.go
+++ green-api/internal/handlers/products_test.go
@@ -11,6 +11,17 @@ import (
 	"github.com/zenichi/green-shop/green-api/internal/utils"
 )
 
+var (
+	SimpleGetProductsRequest = httptest.NewRequest("GET", "/products", nil)
+)
+
+func runRequest(t *testing.T, srv http.Handler, r *http.Request) *httptest.ResponseRecorder {
+	response := httptest.NewRecorder()
+	srv.ServeHTTP(response, r)
+
+	return response
+}
+
 // InMemoryProductData implements data.ProductData interface
 type InMemoryProductData struct{}
 
@@ -20,17 +31,19 @@ func (d *InMemoryProductData) GetProducts() data.Products {
 func (d *InMemoryProductData) AddProduct(p *data.Product) {}
 
 func TestGetProductsAsValidJSON(t *testing.T) {
+	// create dummy store
 	ds := &InMemoryProductData{}
 
-	ih := NewProduct(logrus.WithField("context", "tests"), ds)
+	// create handler
+	ph := NewProduct(logrus.WithField("context", "tests"), ds)
 
-	r := httptest.NewRequest(http.MethodGet, "/products", nil)
-	response := httptest.NewRecorder()
-	ih.ServeHTTP(response, r)
+	// run request
+	response := runRequest(t, ph, SimpleGetProductsRequest)
 
+	// asserts response
 	assert.Equal(t, response.Code, http.StatusOK, "status should be 200")
 
-	p := make(data.Products, 0, 1)
+	var p data.Products
 	err := utils.FromJSON(&p, response.Body)
 	assert.NoError(t, err)
 
