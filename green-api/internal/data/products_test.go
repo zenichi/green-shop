71e6337fd4d330f751217d69e48869f4f004952b
zenichi <krzysztof.kaczmarczyk@protonmail.com>
2022-07-16 19:18:32 +0200

    :traffic_light: add some test scenarios as a sample

---
+++ green-api/internal/data/products_test.go
@@ -0,0 +1,41 @@
+package data
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProductMissingNameReturnsError(t *testing.T) {
+	p := &Product{
+		ID:          1,
+		Name:        "",
+		Description: "Spicy Chinese garlic",
+		Price:       1.50,
+		Currency:    "USD",
+		ExternalID:  "G-45646",
+	}
+
+	v := NewValidator()
+	errors := v.Validate(p)
+
+	assert.Len(t, errors, 1)
+	assert.Contains(t, errors[0], "Product.Name")
+}
+
+func TestProductPriceLowerThanZeroReturnsError(t *testing.T) {
+	p := &Product{
+		ID:          1,
+		Name:        "Garlic",
+		Description: "Spicy Chinese garlic",
+		Price:       -1.50,
+		Currency:    "USD",
+		ExternalID:  "G-45646",
+	}
+
+	v := NewValidator()
+	errors := v.Validate(p)
+
+	assert.Len(t, errors, 1)
+	assert.Contains(t, errors[0], "Product.Price")
+}
